70011f058e3d87e7e7cd8be400d8ae76d9a8fab3
PlsDrop <60732228+PlsDrop@users.noreply.github.com>
2021-03-25 17:57:11 +0200

    Add files via upload

---
+++ collection 25.03/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collection
+{   
+    class Word
+    {
+        public Word(string text_)
+        {
+            text = text_;
+        }
+        public int counter = 1;
+        public string text = ""; 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "I need food, food is my life! Life bad without food, i love food.";
+            //Console.WriteLine(WordFrequency(text));
+            
+            List<Word> list = WordFrequency(text);
+        }
+
+        static List<Word> WordFrequency(string text)
+        {   
+            string tempText = "";
+            string[] massText;
+            
+            int counter = 0;
+            
+            List<Word> list = new List<Word>();
+            
+            text = text.ToLower();
+            massText = text.Split(" ");
+            
+            
+            foreach (string element in massText)
+            {
+                tempText = "";
+                
+                for (int i = 0; i < element.Length; i++)
+                    if (Char.IsLetter(element, i))
+                        tempText += element[i];
+
+                massText[counter] = tempText;
+                counter++;
+            }
+            
+            bool check = false;
+            foreach (string element in massText)
+            {   
+                check = false;
+                foreach (Word wordElement in list)
+                {
+                    if (wordElement.text == element)
+                    {
+                        wordElement.counter++;
+                        check = true;
+                    }
+                }
+                if (!check)
+                    list.Add(new Word(element));
+            }
+            return list;
+        }
+    }
+}
---
+++ employee 25.03/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace employee
+{
+    class Employer : IComparable<Employer>
+    {
+        public Employer(string name_, DateTime birthDay_)
+        {
+            name = name_;
+            birthDay = birthDay_;
+            AgeCalc();
+        }
+        public string name;
+        public DateTime birthDay; 
+
+        public int age;
+
+        private void AgeCalc()
+        {
+            age = (DateTime.Today.Year - birthDay.Year - 1) +
+            (((DateTime.Today.Month > birthDay.Month) || 
+            ((DateTime.Today.Month == birthDay.Month) && (DateTime.Today.Day >= birthDay.Day))) ? 1 : 0);
+        }
+        public int CompareTo(Employer p)
+        {
+            int i = this.birthDay.Month.CompareTo(p.birthDay.Month);
+            if (i == 0)
+            {
+                return this.birthDay.Day.CompareTo(p.birthDay.Day);
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+    }
+    
+    class ListOfEmployee<T> : List<T>
+    {
+        public DateTime today = DateTime.Today;  
+        // public DateTime nextMonth = DateTime.Today.AddMonths(1);  
+        // public DateTime nextMonth2 = DateTime.Today.AddMonths(2);
+
+        public string ToString(int monghtLenght, ListOfEmployee<Employer> list)
+        {   
+            string result = "";
+            for (int i = 0; i <= monghtLenght; i++)
+            {
+                result += $"{today.AddMonths(i).ToString("MMMM yyyy")}\n";
+                foreach (Employer employer in list)
+                {
+                    if (employer.birthDay.Month == today.AddMonths(i).Month)
+                        result += $"({employer.birthDay.ToString("dd")}) - {employer.name} ({employer.age} лет)\n";
+                }
+            }
+            return result;
+        }        
+        //куча кода...
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            ListOfEmployee<Employer> listOfEmployee1 = new ListOfEmployee<Employer>();
+            listOfEmployee1.Add(new Employer("Ваня Иванов", new DateTime(1992,03,20)));
+            listOfEmployee1.Add(new Employer("Петя Петров", new DateTime(1995,03,23)));
+            listOfEmployee1.Add(new Employer("Коля Новогодний", new DateTime(1991,04,01)));
+            listOfEmployee1.Add(new Employer("Стас Рождественский", new DateTime(1998,04,07)));
+            listOfEmployee1.Add(new Employer("Антон Павлович", new DateTime(1986,05,03)));
+            listOfEmployee1.Add(new Employer("Денис Какойто", new DateTime(1990,05,25)));
+            Console.WriteLine(listOfEmployee1.ToString(2, listOfEmployee1));
+        }
+    }
+
+    
+}
